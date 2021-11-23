217ab578ae4de5805746fa1a46d873a9bbf25c24
Alexey Klygin <iosdevfarconsoft@gmail.com>
2021-11-23 19:01:34 +0100

    Init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello there!</h1>
+            <h3>This template just works</h3>
+            <p>Last screen rerender: {new Date().toISOString()}</p>
+        </div>
+    )
+}
+
+export default App
